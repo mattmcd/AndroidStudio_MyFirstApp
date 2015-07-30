721b2670b0b93b705df20aa33709cc39b7813536
mattmcd <matt@matt-mcdonnell.com>
2015-07-30 11:13:00 +0100

    Refactor test script into nose testcase.

--- test_app.py
+++ test_app.py
@@ -14,53 +14,63 @@ sys.path.append(ANDROID_VIEW_CLIENT_HOME)
 
 from com.dtmilano.android.viewclient import ViewClient
 
-# Connect to device that is running the test
-# To check which devices are running: adb devices
-# To start an emulator: emulator -avd <device_name>
-#   e.g. emulator -avd my_nexus7_API_22
-# Note that Android/Sdk/{platform-tools,tools} need to be on path
-device, serialno = ViewClient.connectToDeviceOrExit(serialno='emulator-5554')
+class TestAppEndToEnd():
 
-# Install the application package to this device
-with open(os.devnull, 'w') as devnull:
-    subprocess.call(['adb', 'install',
-    'app/build/outputs/apk/app-debug.apk'], stdout=devnull, stderr=devnull)
+    def __init__(self):
+        self.device = None
+        self.serialno = None
+        # Set package and activity to be started
+        self.package = 'com.mycompany.myfirstapp'
+        self.activity = '.MyActivity'
+        self.runComponent = self.package +'/' + self.activity
 
+    def setUp(self):
+        # Connect to device that is running the test
+        # To check which devices are running: adb devices
+        # To start an emulator: emulator -avd <device_name>
+        #   e.g. emulator -avd my_nexus7_API_22
+        # Note that Android/Sdk/{platform-tools,tools} need to be on path
+        self.device, self.serialno = ViewClient.connectToDeviceOrExit(
+            serialno='emulator-5554')
+        self.device.unlock()
 
-try:
-    # Set package and activity to be started
-    package = 'com.mycompany.myfirstapp'
-    activity = '.MyActivity'
-    runComponent = package +'/' + activity
+        # Install the application package to this device
+        with open(os.devnull, 'w') as devnull:
+            subprocess.call(['adb', 'install',
+            'app/build/outputs/apk/app-debug.apk'], 
+            stdout=devnull, stderr=devnull)
 
-    # Run the activity on the device
-    device.startActivity(component=runComponent)
+    def testEnterTextAndSendMessage(self):
 
-    # Pause
-    time.sleep(2)
-    activityName = device.getTopActivityName()
+        # Run the activity on the device
+        self.device.startActivity(component=self.runComponent)
 
-    vc = ViewClient(device, serialno)
-    vc.dump()
+        # Pause
+        time.sleep(1)
+        activityName = self.device.getTopActivityName()
 
-    editMsg = vc.findViewByIdOrRaise( package + ':' + 'id/edit_message')
-    editMsg.touch()
-    editMsg.type('Hello world!')
-    
-    # Pause
-    time.sleep(1)
+        vc = ViewClient(self.device, self.serialno)
+        vc.dump()
 
-    buttonSend = vc.findViewByIdOrRaise( package + ':' + 'id/button_send')
-    buttonSend.touch();
+        editMsg = vc.findViewByIdOrRaise( 
+            self.package + ':' + 'id/edit_message')
+        editMsg.touch()
+        editMsg.type('Hello world!')
+        
+        # Pause
+        time.sleep(1)
 
-    # Pause
-    time.sleep(2)
-    if not (device.getTopActivityName() == activityName) :
-        # Activity crashed
-        print 'Crashed!'
+        buttonSend = vc.findViewByIdOrRaise( 
+            self.package + ':' + 'id/button_send')
+        buttonSend.touch();
 
-finally:
-    # Uninstall 
-    with open(os.devnull, 'w') as devnull:
-        subprocess.call(['adb', 'uninstall', package], stdout=devnull,
-        stderr=devnull);
+        # Pause
+        time.sleep(1)
+        assert(self.device.getTopActivityName() == activityName)
+
+    def tearDown(self):
+        # Uninstall 
+        with open(os.devnull, 'w') as devnull:
+            subprocess.call(
+            ['adb', 'uninstall', self.package], stdout=devnull,
+            stderr=devnull);
